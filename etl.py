5717d00fc1d3d4725ecfeaa2bbf175ae12ba9b4d
Felipe Simione <felipesimione@gmail.com>
2025-04-08 11:11:34 -0300

    Criacao do requirement e log dentro do etl

--- etl.py
+++ etl.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import os
 import glob
+from log import log_decorator
 
-
+@log_decorator
 def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
     arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
     df_total = pd.concat(df_list, ignore_index=True)
     return df_total
 
+@log_decorator
 def calcular_kpi_total_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df["Total"] = df["Quantidade"] * df["Venda"]
     return df
 
+@log_decorator
 def carregar_dados(df: pd.DataFrame, format_saida: list):
     for formato in format_saida:
         if formato == 'csv':
@@ -20,6 +23,7 @@ def carregar_dados(df: pd.DataFrame, format_saida: list):
         elif formato == 'parquet':
             df.to_parquet('dados.parquet', index=False)
 
+@log_decorator
 def pipeline_calcular_kpi_vendas_consolidado(pasta: str, formato_de_saida: list):
     data_frame = extrair_dados_e_consolidar(pasta)
     data_frame_calculado = calcular_kpi_total_vendas(data_frame)
